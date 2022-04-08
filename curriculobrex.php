d8e1b2bfab976a4aae8e3d2336361c3365c0b78c
Estefanosb <87233013+Estefanosb@users.noreply.github.com>
2022-04-08 18:57:54 -0300

    Add files via upload

---
+++ curriculobrex.php
@@ -0,0 +1,86 @@
+<html>
+  <head>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title> Currículo</title>
+    
+	<style rel="stylesheet" type="text/css">
+
+html, body{
+	margin: 20;
+	padding: 20;
+	font-family: "Helvetica", sans-serif;
+	font-size: 14pt;
+	background-color:#ccefff;
+	line-height: 1.5;
+}
+
+
+
+
+
+
+hr{
+  border-top: 10px solid #000066;
+}
+
+
+h2 {
+	color: #000066;
+	font-family: arial; 
+
+	margin-top:40px;
+
+}
+
+hr{
+ line-height:0px;
+ text-align: justify;
+ text-justify: inter-word;
+ padding: 5px;
+
+}
+
+	
+
+</style>
+  </head>
+  <body>
+  
+    <h1>Breno Rennó Santos Araujo</h1>
+	
+    <hr>
+	
+    <h2 style="color:#000066;">Informações:</h2>
+	
+    <b>Email:</b> araujobreno@outlook.com.br <br>
+    <b>Idade:</b> 18 <br>
+    <b>Estado:</b> Espírito Santo <br>
+    <b>Cidade:</b> Vitória <br>
+    <b>Telefone:</b> (27)997250540 <br><br><br>
+	
+    <hr>
+	
+    <h2 style="color:#000066;">Formação acadêmica:</h2>
+	
+    <p>
+	
+	<b> Ensino Médio:</b> Completo<br>
+	<b> Escola de formação:</b> Escola São Domingos<br>
+	<b>Graduação:</b> Ciências de computação - UVV <br>
+	<b>Habilidades: </b> Programação, Web Development,  Modelagem de banco de dados <br></p><br>
+	
+	 <hr>
+	
+    <h2 style="color:#000066;">Idiomas falados</h2>
+	
+    <li>Português</li>
+    <li>Inglês</li>
+	
+	
+    <script src="script.js"></script>
+  </body>
+  <?php
+  ?>
+</html>
